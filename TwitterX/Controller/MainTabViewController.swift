8c13afd024f9b98551a27d0d6f9fc4475323a5b2
sourav-singh-rawat <souravrawat1999@gmail.com>
2022-12-22 23:26:06 +0530

    remove main storyboard files

---
+++ TwitterX/Controller/MainTabViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabViewController.swift
+//  TwitterX
+//
+//  Created by Sourav Singh Rawat on 22/12/22.
+//
+
+import UIKit
+
+class MainTabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+}
