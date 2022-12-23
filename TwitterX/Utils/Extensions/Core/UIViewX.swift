d82ede3bbb90e86d9d48b592da54758995d89a07
sourav-singh-rawat <souravrawat1999@gmail.com>
2022-12-23 16:10:47 +0530

    Add constraints helper extension

--- TwitterX/Utils/Extensions/Core/UIViewX.swift
+++ TwitterX/Utils/Extensions/Core/UIViewX.swift
@@ -9,38 +9,59 @@ import UIKit
 
 extension UIView {
     func padding(
-        to: UIView,
-        insets: UIEdgeInsets,
-        withSafeArea: Bool = false
+        to view: UIView,
+        withInsets insets: UIEdgeInsets,
+        withSafeAreaProtected safeAreaProtected: Bool = false
     ){
         translatesAutoresizingMaskIntoConstraints = false
         
-        NSLayoutConstraint.activate([
-            self.topAnchor.constraint(
-                equalTo: withSafeArea
-                    ?to.safeAreaLayoutGuide.topAnchor
-                    :to.topAnchor,
-                constant: insets.top ?? 0
-            ),
-            self.leadingAnchor.constraint(
-                equalTo: withSafeArea
-                    ?to.safeAreaLayoutGuide.leadingAnchor
-                    :to.leadingAnchor,
-                constant: insets.left ?? 0
-            ),
-            self.trailingAnchor.constraint(
-                equalTo: withSafeArea
-                    ?to.safeAreaLayoutGuide.trailingAnchor
-                    :to.trailingAnchor,
-                constant: -(insets.right ?? 0)
-            ),
-            self.bottomAnchor.constraint(
-                equalTo: withSafeArea
-                    ?to.safeAreaLayoutGuide.bottomAnchor
-                    :to.bottomAnchor,
-                constant: -(insets.bottom ?? 0)
+        var constraints: [NSLayoutConstraint] = []
+        
+        if insets.top != 0 {
+            constraints.append(
+                self.topAnchor.constraint(
+                    equalTo: safeAreaProtected
+                    ?view.safeAreaLayoutGuide.topAnchor
+                    :view.topAnchor,
+                    constant: insets.top
+                )
             )
-        ])
+        }
+        
+        if insets.left != 0 {
+            constraints.append(
+                self.leadingAnchor.constraint(
+                    equalTo: safeAreaProtected
+                        ?view.safeAreaLayoutGuide.leadingAnchor
+                        :view.leadingAnchor,
+                    constant: insets.left
+                )
+            )
+        }
+        
+        if insets.right != 0 {
+            constraints.append(
+                self.trailingAnchor.constraint(
+                    equalTo: safeAreaProtected
+                        ?view.safeAreaLayoutGuide.trailingAnchor
+                        :view.trailingAnchor,
+                    constant: -(insets.right)
+                )
+            )
+        }
+        
+        if insets.bottom != 0 {
+            constraints.append(
+                self.bottomAnchor.constraint(
+                    equalTo: safeAreaProtected
+                        ?view.safeAreaLayoutGuide.bottomAnchor
+                        :view.bottomAnchor,
+                    constant: -(insets.bottom)
+                )
+            )
+        }
+        
+        NSLayoutConstraint.activate(constraints)
     }
     
     func width(_ width: CGFloat){
@@ -68,12 +89,44 @@ extension UIView {
 }
 
 struct UIEdgeInsets {
-    let left: CGFloat?
-    let right: CGFloat?
-    let bottom: CGFloat?
-    let top: CGFloat?
+    let left: CGFloat
+    let right: CGFloat
+    let bottom: CGFloat
+    let top: CGFloat
     
-    var zero: UIEdgeInsets {
-        return UIEdgeInsets(left: 0, right: 0, bottom: 0, top: 0)
+    static var zero: UIEdgeInsets {
+        return UIEdgeInsets(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0
+        )
+    }
+    
+    static func symmetric(horizontal: CGFloat? = nil,vertical: CGFloat? = nil) -> UIEdgeInsets {
+        return UIEdgeInsets(
+            left: horizontal ?? 0,
+            right: horizontal ?? 0,
+            bottom: vertical ?? 0,
+            top: vertical ?? 0
+        )
+    }
+    
+    static func all(_ value: CGFloat) -> UIEdgeInsets{
+        return UIEdgeInsets(
+            left: value,
+            right: value,
+            bottom: value,
+            top: value
+        )
+    }
+    
+    static func only(left: CGFloat? = nil, right: CGFloat? = nil, bottom: CGFloat? = nil, top: CGFloat? = nil) -> UIEdgeInsets {
+        return UIEdgeInsets(
+            left: left ?? 0,
+            right: right ?? 0,
+            bottom: bottom ?? 0,
+            top: top ?? 0
+        )
     }
 }
