922c1235199e4e0d4e5cea8deea0acd519ef8cd8
sourav-singh-rawat <souravrawat1999@gmail.com>
2022-12-29 13:32:33 +0530

    Add repository basic protocols

---
+++ TwitterX/Repository/TXRepositoryProtocol.swift
@@ -0,0 +1,46 @@
+//
+//  TXRepositoryProtocol.swift
+//  TwitterX
+//
+//  Created by Sourav Singh Rawat on 28/12/22.
+//
+
+import Foundation
+
+//MARK: - Repository Protocol
+
+protocol TXRepositoryProtocol {
+    
+}
+
+//MARK: - Request Protocol
+
+protocol TXRepositoryRequest{
+    func toPayload() -> [String:Any]
+}
+
+//MARK: - Response Protocol
+
+protocol TXRepositoryFailureProtocol {
+    var message: String { get set }
+}
+
+extension TXRepositoryFailureProtocol {
+    var message: String {
+        get {
+            return "😯 Oops! Something went wrong."
+        }
+    }
+}
+
+protocol TXRepositorySuccessProtocol {
+    var message: String { get set }
+}
+
+extension TXRepositorySuccessProtocol {
+    var message: String {
+        get {
+            return "Success"
+        }
+    }
+}
