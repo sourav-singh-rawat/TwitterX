e727adbce13bc9b58de74999ae3c811237c0dfd3
sourav-singh-rawat <souravrawat1999@gmail.com>
2023-01-04 15:24:17 +0530

    Make TXUser model decodeable

--- TwitterX/Repository/protocol/User/Model/TXUser.swift
+++ TwitterX/Repository/protocol/User/Model/TXUser.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-struct TXUser {
+class TXUser: Decodable {
     let uid: String
     var profileImage: UIImage?
     let profileImageUrl: String
@@ -15,4 +15,32 @@ struct TXUser {
     var password: String?
     let fullname: String
     let username: String
+    
+    init(uid: String, profileImage: UIImage? = nil, profileImageUrl: String, email: String, password: String? = nil, fullname: String, username: String) {
+        self.uid = uid
+        self.profileImage = profileImage
+        self.profileImageUrl = profileImageUrl
+        self.email = email
+        self.password = password
+        self.fullname = fullname
+        self.username = username
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case uid
+        case profileImageUrl
+        case email
+        case fullname
+        case username
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        uid = try values.decode(String.self, forKey: .uid)
+        profileImageUrl = try values.decode(String.self, forKey: .profileImageUrl)
+        email = try values.decode(String.self, forKey: .email)
+        fullname = try values.decode(String.self, forKey: .fullname)
+        username = try values.decode(String.self, forKey: .username)
+    }
 }
