82f2b05f5c6973f6744cdc2244d013e11b0a7bd3
sourav-singh-rawat <souravrawat1999@gmail.com>
2023-01-04 15:25:10 +0530

    Complete sign-in & get-user-details feature

--- TwitterX/Repository/data/User/TXUserRepository.swift
+++ TwitterX/Repository/data/User/TXUserRepository.swift
@@ -111,7 +111,7 @@ class TXUserRepository: TXUserRepositoryProtocol {
         Auth.auth().signIn(
             withEmail: request.email,
             password: request.password
-        ) { result, error in
+        ) { [weak self] result, error in
             if result == nil || error != nil {
                 completion(.failure(
                     TXLoginUserFailure(
@@ -122,23 +122,62 @@ class TXUserRepository: TXUserRepositoryProtocol {
             
             guard let uuid = result?.user.uid else { return }
             
-            
+            self?.getUserDetails(
+                with: TXGetUserDetailsRequest(uuid: uuid),
+                completion: { result in
+                    switch result {
+                    case .success(let response):
+                        completion(.success(
+                            TXLoginUserSuccess(user: response.user)
+                        ))
+                        break
+                    case .failure(let response):
+                        completion(.failure(
+                            TXLoginUserFailure(
+                                localizedDescription: response.localizedDescription
+                            )
+                        ))
+                        break
+                    }
+                }
+            )
         }
     }
     
     func getUserDetails(with request: TXGetUserDetailsRequest,completion: @escaping GetUserDetailsCompletion) {
-//        Firestore
-//            .firestore()
-//            .collection("Users")
-//            .getDocuments { snapshot, error in
-//                if snapshot == nil || error != nil {
-//                    self.delegate?.didGetUserDetailsFailed(
-//                        response: TXGetUserDetailsFailure(
-//                            message: error?.localizedDescription ?? "Failed to get user details"
-//                        )
-//                    )
-//                }
-//
-//            }
+        Firestore
+            .firestore()
+            .collection("Users")
+            .document(request.uuid)
+            .getDocument { document, error in
+                if let document = document, document.exists, error == nil {
+                    
+                    guard let firestoreObj = document.data() else {
+                        completion(.failure(
+                            TXGetUserDetailsFailure(
+                                localizedDescription: "Data not stored for this user"
+                            )
+                        ))
+                        return
+                    }
+                    
+                    do {
+                        let data = try JSONSerialization.data(withJSONObject: firestoreObj, options: .prettyPrinted)
+                       
+                        let decoder = JSONDecoder()
+                        let user = try decoder.decode(TXUser.self, from: data)
+                        
+                        completion(.success(TXGetUserDetailsSuccess(user: user)))
+                    } catch {
+                        assertionFailure("Failed to transform data")
+                    }
+                }else {
+                    completion(.failure(
+                        TXGetUserDetailsFailure(
+                            localizedDescription: error?.localizedDescription ?? "Failed to get user details"
+                        )
+                    ))
+                }
+            }
     }
 }
