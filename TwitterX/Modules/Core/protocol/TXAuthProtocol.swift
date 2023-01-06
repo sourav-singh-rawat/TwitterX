ef650f19b8b16461885c221560e33e42197124aa
sourav-singh-rawat <souravrawat1999@gmail.com>
2023-01-06 06:46:50 +0530

    WIP: Add auth module

---
+++ TwitterX/Modules/Core/protocol/TXAuthProtocol.swift
@@ -0,0 +1,20 @@
+//
+//  TXAuthProtocol.swift
+//  TwitterX
+//
+//  Created by Sourav Singh Rawat on 04/01/23.
+//
+
+import Foundation
+
+protocol TXAuthProtocol {
+    var isAuthenticated: Bool { get }
+    
+    func onLogin(uuid: String)
+    
+    func onLogout()
+    
+    var currentUser: TXUser? { get }
+    
+    func refreshUser()
+}
